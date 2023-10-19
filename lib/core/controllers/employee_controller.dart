39b40c20114e5c11ac1ec072e38fea1839c6dc22
Oseni Adewale <adewaleoseni6@gmail.com>
2023-10-19 17:46:48 +0100

    completing employment details

--- lib/core/controllers/employee_controller.dart
+++ lib/core/controllers/employee_controller.dart
@@ -1,21 +1,97 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mobile_assessment/core/models/employee.dart';
 
-class EmployeeController extends GetxController{
+class EmployeeController extends GetxController {
+  Color getEmploymentStatusColor(int level, double productivityScore) {
+    if (level == 0) {
+      // Level 0 employees cannot be demoted, only terminated if need be
+      if (productivityScore >= 80.0) {
+        return Colors.green;
+      } else if (productivityScore <= 39.0) {
+        return Colors.red;
+      } else {
+        return Colors.grey;
+      }
+    } else if (level == 5 && productivityScore >= 80.0) {
+      return Colors.grey;
+    } else {
+      // For levels 1 to 5
+      if (productivityScore >= 80.0) {
+        return Colors.green;
+      } else if (productivityScore >= 50.0) {
+        return Colors.grey;
+      } else if (productivityScore >= 40.0) {
+        return Colors.brown;
+      } else {
+        return Colors.red;
+      }
+    }
+  }
+
+  String getEmploymentStatus(int level, double productivityScore) {
+    /// Check if the employee's level is 0
+    /// Level 0 employees cannot be demoted, only terminated if need be
+    if (level == 0) {
+      if (productivityScore >= 80.0) {
+        return 'Promoted';
+      } else if (productivityScore <= 39.0) {
+        return 'Termination';
+      } else {
+        return 'No Change';
+      }
+    }
+    /// because the highest level is 5, there is no need promoting an employee
+    /// that has a productivity score greater than 79
+    else if (level == 5 && productivityScore >= 80.0) {
+      return 'No Change';
+    } else {
+      // For levels 1 to 5
+      if (productivityScore >= 80.0) {
+        return 'Promoted';
+      } else if (productivityScore >= 50.0) {
+        return 'No Change';
+      } else if (productivityScore >= 40.0) {
+        return 'Demoted';
+      } else {
+        return 'Terminated';
+      }
+    }
+  }
+
+  String calculateNewSalary(int level) {
+    switch (level) {
+      case 0:
+        return "70,000";
+      case 1:
+        return "100,000";
+      case 2:
+        return "120,000";
+      case 3:
+        return "180,000";
+      case 4:
+        return "200,000";
+      case 5:
+        return "250,000";
+      default:
+        return "Salary not specified";
+    }
+  }
 
   List<Employee> filterEmployees(
-      List<Employee> employees,
-      String searchText,
-      String filterBy,
-      ) {
+    List<Employee> employees,
+    String searchText,
+    String filterBy,
+  ) {
 
-    /// Normalize the search text to lowercase for a case-insensitive search
     final normalizedSearchText = searchText.toLowerCase();
 
     return employees.where((employee) {
       /// Check if the employee's name, designation, or level contains the search text
+      /// the time complexity for this iteration is O(n * m), where "n" is the number of employees in the list and "m" is the length of the longest string involved in the filtering operation.
       if (filterBy == "Name") {
-        final fullName = "${employee.firstName} ${employee.lastName}".toLowerCase();
+        final fullName =
+            "${employee.firstName} ${employee.lastName}".toLowerCase();
         return fullName.contains(normalizedSearchText);
       } else if (filterBy == "Designation") {
         final normalizedDesignation = employee.designation!.toLowerCase();
@@ -28,4 +104,19 @@ class EmployeeController extends GetxController{
     }).toList();
   }
 
+  bool isPromoted(double productivityScore, int level) {
+    return level < 5 && productivityScore >= 80.0;
+  }
+
+  bool isDemoted(double productivityScore, int level) {
+    return level > 0 && (productivityScore >= 40 && productivityScore <= 49);
+  }
+
+  String getActiveStatus(int? employmentStatus) {
+    if (employmentStatus == 1) {
+      return 'Active';
+    } else {
+      return 'InActive';
+    }
+  }
 }
--- lib/ui/views/details/employee_detail.dart
+++ lib/ui/views/details/employee_detail.dart
@@ -70,31 +70,97 @@ class EmployeeDetail extends StatelessWidget {
                   ))
                 ],
               ),
-              Text(
-                'Level: ${employee.level}',
-                style: TextStyle(fontSize: 20),
+              const SizedBox(height: 10),
+              const Divider(),
+              const SizedBox(height: 10),
+
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Employment Status',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: controller.getEmploymentStatusColor(
+                        employee.level!,
+                        employee.productivityScore!,
+                      ),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(8),
+                      ),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      controller.getEmploymentStatus(
+                        employee.level!,
+                        employee.productivityScore!,
+                      ),
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
               ),
-              SizedBox(height: 16),
-              Divider(),
-              SizedBox(height: 16),
-              Text(
-                'Details:',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+
+              ///update new salary when employee is promoted
+              if (controller.isPromoted(
+                  employee.productivityScore!, employee.level!))
+                _buildPromotedSalary(context),
+
+              ///update new salary when employee is demoted
+              if (controller.isDemoted(
+                  employee.productivityScore!, employee.level!))
+                _buildDemotedSalary(context),
+
+              const SizedBox(height: 10),
+              _buildEmployeeDetailsCard(context)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildEmployeeDetailsCard(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Card(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(8),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildCardItem('Productivity Score: ',
+                  '${employee.productivityScore}', context),
+              const SizedBox(
+                height: 10,
               ),
-              Text(
-                'Productivity Score: ${employee.productivityScore?.toStringAsFixed(2)}',
-                style: TextStyle(fontSize: 18),
+              _buildCardItem(
+                  'Current Salary: ', '${employee.currentSalary}', context),
+              const SizedBox(
+                height: 10,
               ),
-              Text(
-                'Current Salary: ${employee.currentSalary}',
-                style: TextStyle(fontSize: 18),
+              _buildCardItem('Current Level: ', '${employee.level}', context),
+              const SizedBox(
+                height: 10,
               ),
-              Text(
-                'Employment Status: ${getEmploymentStatusText(employee.employmentStatus)}',
-                style: TextStyle(fontSize: 18),
+              _buildCardItem(
+                  'Status: ',
+                  controller.getActiveStatus(employee.employmentStatus),
+                  context),
+              const SizedBox(
+                height: 10,
               ),
             ],
           ),
@@ -103,11 +169,43 @@ class EmployeeDetail extends StatelessWidget {
     );
   }
 
-  String getEmploymentStatusText(int? employmentStatus) {
-    if (employmentStatus == 1) {
-      return 'Active';
-    } else {
-      return 'Terminated';
-    }
+  _buildCardItem(String title, String content, context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            title,
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        Text(
+          content,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+      ],
+    );
+  }
+
+  _buildDemotedSalary(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 10),
+        _buildCardItem('New Salary',
+            controller.calculateNewSalary(employee.level! - 1), context),
+      ],
+    );
+  }
+
+  _buildPromotedSalary(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 10),
+        _buildCardItem('New Salary',
+            controller.calculateNewSalary(employee.level! + 1), context),
+      ],
+    );
   }
 }
